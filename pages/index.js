642fc601d5430a3b669be1b875ac13fd15c5ca71
FilipeZampieri <filipeczampieri@gmail.com>
2025-01-26 14:36:54 +0000

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 function Home() {
-    return <h1> Antes de conhecer você eu achava que sabia oque é companheirismo e amor, mas você me mostrou que eu não sabia de nada.
-    Você é a pessoa mais incrível que eu já conheci, e eu sou muito grato por ter você na minha vida.
-    Eu te amo muito Leidy, e você é a pessoal mais importante na minha vida. ❤💕</h1>;
+  return (
+    <h1>
+      Antes de conhecer você eu achava que sabia oque é companheirismo e amor,
+      mas você me mostrou que eu não sabia de nada. Você é a pessoa mais
+      incrível que eu já conheci, e eu sou muito grato por ter você na minha
+      vida. Eu te amo muito Leidy, e você é a pessoal mais importante na minha
+      vida. ❤💕
+    </h1>
+  );
 }
 export default Home;
